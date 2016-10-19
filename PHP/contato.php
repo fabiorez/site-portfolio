41f4cd27797f87d0c3bd321490570cfafa5e19cc
Fabio Rezende <fabio.rezende@gmail.com>
2016-10-19 17:08:02 -0300

    preparando os arquivos para o wordpress

---
+++ PHP/contato.php
@@ -0,0 +1,6 @@
+<?php
+    $style = 'contato'; 
+    $chamada = '<strong>Entre</strong> em <strong>contato</strong>'; 
+    include('header.php');
+    include('includes/pages/contato.php');
+    include('footer.php');

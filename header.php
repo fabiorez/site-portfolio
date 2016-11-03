dbaeddc3b638389a8e627c417d4d8072a5b6e6f1
Fabio Rezende <fabio.rezende@gmail.com>
2016-11-03 09:28:43 -0300

    -adicionado php

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Site Fabio Rezende</title>
+	<link rel="stylesheet" href="css/<?php echo $style; ?>.css">
+	<link rel="stylesheet" href="css/geral.css">
+	<link rel="stylesheet" href="bower_components/wow/css/libs/animate.css">
+	<link rel="stylesheet" href="css/plugins.css">
+	
+</head>
+<body> 
+
+	<?php include('includes/organisms/menu-responsivo.php') ?>
+
+	<div class="layout">
+		<header>
+			<div class="container">
+				<div class="logo">
+					<a href="index.html">
+						<img src="images/logo-fr-topo.svg" alt="Logo Topo Fr">
+					</a>
+				</div>
+
+				<div class="links">
+					<?php include('includes/organisms/menu.php') ?>
+
+					
+					<a href="javascript:;" class="toggle">
+						<span></span>
+						<span></span>
+						<span></span>
+					</a>
+
+					<ul class="social">
+						<li>
+							<a href="#"><i class="fa fa-facebook"></i></a>
+						</li>
+						<li>
+							<a href="#"><i class="fa fa-linkedin"></i></a>
+						</li>
+					</ul>
+				</div>
+
+				<h1 class="wow pulse" data-wow-duration="1s" data-wow-delay="1s"><?php echo $chamada; ?></h1>
+			</div>
+		</header>
+
+		<main>
---
+++ portfolio.php
@@ -0,0 +1,6 @@
+<?php
+    $style = 'portfolio';  
+    $chamada = 'Meus<strong> trabalhos</strong>';
+    include('header.php');
+    include('includes/pages/portfolio.php');
+    include('footer.php');

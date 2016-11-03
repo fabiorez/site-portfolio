eee697129c5305661d61cf153c307a3b445d0f84
Fabio Rezende <fabio.rezende@gmail.com>
2016-11-03 09:33:23 -0300

    adicionado wordpress

---
+++ footer.php
@@ -0,0 +1,26 @@
+		</main>
+
+		<footer>
+			<div class="logo-rodape">
+				<a href="<?php bloginfo('url');?>">
+					<img src="<?php bloginfo('template_url');?>/images/logo-fr-rodape.svg" alt="Logo Rodape Fr">
+				</a>
+			</div>
+			<p><?php echo date('Y');?> Todos os direitos reservados.  <a href="mailto:<?php bloginfo('admin_email'); ?>"><?php bloginfo('admin_email'); ?></a></p>
+		</footer>
+	</div>
+
+	<script src="<?php bloginfo('template_url');?>/bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="<?php bloginfo('template_url');?>/bower_components/wow/dist/wow.min.js"></script>
+	<script>
+		$(function() {
+			$('.toggle').click(function() {
+				$('.layout').toggleClass('ativo');
+				$('.menu-responsivo').toggleClass('ativo');
+				$(this).toggleClass('ativo');
+			});
+			new WOW().init();
+		});
+	</script>
+</body>
+</html>
---
+++ includes/pages/sobre.php
@@ -0,0 +1,35 @@
+<section class="sobre">
+    <div class="container">
+        <div class="textos">
+            <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong> Sit laborum suscipit animi tenetur non dolores maiores, omnis, quo minus, obcaecati quasi nemo voluptatem deleniti amet incidunt maxime ab eligendi dolore.</p>
+        </div>
+        <ul class="habilidades">
+            <li class="habilidade-5">
+                <h2>
+                    <i class="fa fa-html5"></i> <strong>HTML5</strong> // 40%
+                    <div class="barra"><span></span></div>
+                </h2>
+            </li>
+            <li class="habilidade-3">
+                <h2>
+                    <i class="fa fa-css3"></i> <strong>CSS/JAVASCRIPT</strong> // 30%
+                    <div class="barra"><span></span></div>
+                </h2>
+            </li>
+            <li class="habilidade-4">
+                <h2>
+                    <i class="fa fa-hashtag"></i> <strong>C#</strong> // 40%
+                    <div class="barra"><span></span></div>
+                </h2>
+            </li>
+            <li class="habilidade-5">
+                <h2>
+                    <i class="fa fa-database"></i> <strong>SQL/ORACLE</strong> // 50%
+                    <div class="barra"><span></span></div>
+                </h2>
+            </li>
+        </ul>
+
+            <a href="portfolio.html" class="botao medio"><i class="fa fa-briefcase"></i> Acessar Portfolio</a>
+    </div>
+</section>
